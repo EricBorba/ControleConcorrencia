99db03686583ab90361f90f10e807ff000bc3365
Familia Dantas <Familia Dantas@FamiliaDantas>
2012-05-28 18:07:50 -0300

    classe lock multiplo iniciada,falta completar o acrescimo e decrescimo

---
+++ src/GerenciaDeDados/BloqueioLockMultiplo.java
@@ -0,0 +1,43 @@
+package GerenciaDeDados;
+
+public class BloqueioLockMultiplo {
+
+	String NomeTransacao;
+	String NomeVariavel;
+	String modoBloqueio;
+
+	public BloqueioLockMultiplo(String NomeTransacaoNova,String NomeVariavelNova,String modoBloqueioNovo){
+
+		this.NomeTransacao = NomeTransacaoNova;
+		this.NomeVariavel = NomeVariavelNova;
+		this.modoBloqueio = modoBloqueioNovo;
+
+
+	}
+
+	public String getNomeTransacao() {
+		return NomeTransacao;
+	}
+
+	public void setNomeTransacao(String nomeTransacao) {
+		NomeTransacao = nomeTransacao;
+	}
+
+	public String getNomeVariavel() {
+		return NomeVariavel;
+	}
+
+	public void setNomeVariavel(String nomeVariavel) {
+		NomeVariavel = nomeVariavel;
+	}
+
+	public String getModoBloqueio() {
+		return modoBloqueio;
+	}
+
+	public void setModoBloqueio(String modoBloqueio) {
+		this.modoBloqueio = modoBloqueio;
+	}
+
+
+}
--- src/MecanismosDeConcorrencia/LockMultiplo.java
+++ src/MecanismosDeConcorrencia/LockMultiplo.java
@@ -1,5 +1,105 @@
 package MecanismosDeConcorrencia;
 
+import GerenciaDeDados.Operacao;
+import GerenciaDeDados.Repositorio;
+
 public class LockMultiplo {
+	String transacao;
+	Operacao operacao;
+	Repositorio repositorio;
+
+
+	public LockMultiplo(String transacaonova,Operacao operacaoNova,Repositorio repositorioNovo){
+		this.operacao = operacaoNova;
+		this.repositorio = repositorioNovo;
+		this.transacao = transacaonova;
+
+	}
+
+	/**construindo o Lock, se conseguir bloquear retorna 1, se não conseguir bloquear porque a
+	 * variavel estava sendo bloqueada por outra transacao retorna 2, se não bloqueou pq estava sendo bloqueada
+	 * pela mesma transacao retorna 3*/
+
+	public int construindoLockMultiplo(){
+
+		int retorno = 0;
+		int posicaoCrescerBloqueio = 0;
+		int existeOutroBloqueio = 0;//verificar se existe outro read_lock com a mesma variavel
+		
+		//REVER TUDO
+		if(operacao.getNomeOperacao() == "Read"){
+			for(int i = 0; i < this.repositorio.getListaDeBloqueioMultiplo().size(); i++){
+				if(this.repositorio.getListaDeBloqueioMultiplo().get(i).getNomeVariavel().equals(this.operacao.getVariavel())){
+					if(!this.repositorio.getListaDeBloqueioMultiplo().get(i).getNomeTransacao().equals(transacao)){
+						if(this.repositorio.getListaDeBloqueioMultiplo().get(i).getModoBloqueio() == "Read_lock"){
+
+							retorno = 1;
+
+						}else if(this.repositorio.getListaDeBloqueioMultiplo().get(i).getModoBloqueio() == "Write_lock"){
+							/////bloqueada por outra com write_lock
+							retorno = 2;
+
+						}else{
+
+							System.out.println("ERRO,NOME DE BLOQUEIO ERRADO");
+						}
+					}else{
+						///mesma transação
+						retorno = 3;
+
+					}
+				}
+
+			}
+
+			if(retorno == 1){
+				this.repositorio.adicionarBloqueioLista(transacao,operacao,1);
+			}
+
+
+		}else if(operacao.getNomeOperacao() == "Write"){
+
+			for(int i = 0; i < this.repositorio.getListaDeBloqueioMultiplo().size(); i++){
+
+				if(this.repositorio.getListaDeBloqueioMultiplo().get(i).getNomeVariavel().equals(this.operacao.getVariavel())){
+					existeOutroBloqueio =  existeOutroBloqueio + 1;
+					if(this.repositorio.getListaDeBloqueioMultiplo().get(i).getNomeTransacao().equals(transacao)){
+						if(this.repositorio.getListaDeBloqueioMultiplo().get(i).getModoBloqueio().equals("Read_lock")&& existeOutroBloqueio < 2){
+							posicaoCrescerBloqueio = i;
+							
+							retorno = 1;
+						}else if(existeOutroBloqueio > 2){
+
+							retorno = 2;
+						}else{
+							retorno = 3;
+						}
+					}
+
+				}
+
+			}
+			
+			if(existeOutroBloqueio >= 2){
+				retorno = 2;
+			}else if(retorno == 0 && existeOutroBloqueio < 2){
+				retorno = 2;
+			}else if (retorno == 1 && existeOutroBloqueio < 2){
+				this.repositorio.getListaDeBloqueioMultiplo().get(posicaoCrescerBloqueio).setModoBloqueio("Write_lock");
+			}
+
+		}else{
+
+			System.out.println("ERRO,NOME DE OPERAÇÃO ERRADA");
+
+		}
+		return retorno;
+	}
+
+
+
+/// fiz o acrescimo qndo se pede um write e tem um read da transação, falta verificar os outros casos 
+	////acrescimo e decrescimo
+
 
 }
